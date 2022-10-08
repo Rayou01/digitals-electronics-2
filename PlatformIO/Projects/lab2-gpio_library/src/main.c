74b011e3067e1bb26859beeede38c66377e4d45c
Rayou01 <114081889+Rayou01@users.noreply.github.com>
2022-10-08 12:20:39 +0200

    Update main.c

--- PlatformIO/Projects/lab2-gpio_library/src/main.c
+++ PlatformIO/Projects/lab2-gpio_library/src/main.c
@@ -1,6 +1,6 @@
 /***********************************************************************
  * 
- * Blink a LED in Arduino-style and use function from the delay library.
+ * Blink LEDs in Arduino-style and use function from the delay library.
  * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
  *
  * Copyright (c) 2022 Tomas Fryza
@@ -11,23 +11,24 @@
 
 
 /* Defines -----------------------------------------------------------*/
-#define LED_GREEN PB5   // PB5 is AVR pin where green on-board LED 
-#define LED_RED   PB0
+#define LED_GREEN PB5   // PB5 is AVR pin where green on-board LED
                         // is connected
+#define LED_RED PB0     // External active-low LED
 #define SHORT_DELAY 250 // Delay in milliseconds
 #ifndef F_CPU
 # define F_CPU 16000000 // CPU frequency in Hz required for delay funcs
 #endif
 
 /* Includes ----------------------------------------------------------*/
+#include <gpio.h>
 #include <avr/io.h>     // AVR device-specific IO definitions
 #include <util/delay.h> // Functions for busy-wait delay loops
-#include <gpio.h>
 
 
 // -----
 // This part is needed to use Arduino functions but also physical pin
 // names. We are using Arduino-style just to simplify the first lab.
+// #include "Arduino.h"
 #define PB5 13          // In Arduino world, PB5 is called "13"
 #define PB0 8
 // -----
@@ -36,47 +37,66 @@
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
  * Function: Main function where the program execution begins
- * Purpose:  Toggle one LED and use delay library.
+ * Purpose:  Toggle LEDs and use delay library.
  * Returns:  none
  **********************************************************************/
 int main(void)
 {
-    uint8_t led_value = 0;  // Local variable to keep LED status
+  uint8_t led_value = 0;  // Local variable to keep LED status
 
-    // Set pin where on-board LED is connected as output
-    // pinMode(LED_GREEN, OUTPUT);
-    // pinMode(LED_RED, OUTPUT);
+  // Set pins where LEDs are connected as output
 
-    DDRB = DDRB | (1<<LED_GREEN);
-    DDRB = DDRB | (1<<LED_RED);
+  // Version 1 : Arduino-style
+  // pinMode(LED_GREEN, OUTPUT);
+  // pinMode(LED_RED, OUTPUT);
 
-    GPIO_mode_output(&DDRB, LED_GREEN);
-    GPIO_mode_output(&DDRB, LED_RED);
+  // Version 2 : Registers
+  // DDRB = DDRB | (1<<LED_GREEN);
+  // DDRB = DDRB | (1<<LED_RED);
 
-    // Infinite loop
-    while (1)
-    {
-        // Change LED value
-        if (led_value == 0){
-          led_value = 1;
+  // Version 3 : Library functions
+  GPIO_mode_output(&DDRB, LED_GREEN);
+  GPIO_mode_output(&DDRB, LED_RED);
 
-          PORTB = PORTB | (1<<LED_GREEN);
-          PORTB = PORTB | (1<<LED_RED);
+  // Infinite loop
+  while(1){
+    // Pause several milliseconds
+    // _delay_ms(SHORT_DELAY);
 
-          GPIO_write_high(&PORTB, LED_GREEN);
-          GPIO_write_high(&PORTB, LED_RED);
-        }
-        else{
-          led_value = 0;
+    // Change LED value
+    if (led_value == 0){
+      led_value = 1;
+      // Set pin(s) to HIGH
 
-          PORTB = PORTB & ~(1<<LED_GREEN);
-          PORTB = PORTB & ~(1<<LED_RED);
+      // Version 1 : Arduino-style
+      // digitalWrite(LED_GREEN, HIGH);
+      // digitalWrite(LED_RED, HIGH);
 
-          GPIO_write_low(&PORTB, LED_GREEN);
-          GPIO_write_low(&PORTB, LED_RED);
-        }
+      // Version 2 : Registers
+      // PORTB = PORTB | (1<<LED_GREEN);
+      // PORTB = PORTB | (1<<LED_RED);
+
+      // Version 3 : Library functions
+      GPIO_write_high(&PORTB, LED_GREEN);
+      GPIO_write_high(&PORTB, LED_RED);
     }
+    else {
+      led_value = 0;
+      // Clear pin(s) to LOW
 
-    // Will never reach this
-    return 0;
+      // Version 1 : Arduino-style
+      // digitalWrite(LED_GREEN, LOW);
+      // digitalWrite(LED_RED, LOW);
+
+      // Version 2 : Registers
+      // PORTB = PORTB & ~(1<<LED_GREEN);
+      // PORTB = PORTB & ~(1<<LED_RED);
+    
+      // Version 3 : Library functions
+      GPIO_write_low(&PORTB, LED_GREEN);
+      GPIO_write_low(&PORTB, LED_RED);
+    }
+  }
+  // Will never reach this
+  return 0;
 }
